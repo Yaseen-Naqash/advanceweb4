f58fd17973b9e7ce20c3e39b7a2d5f299fe431d1
Yaseen <yasin.naqash.1999@gmail.com>
2025-12-16 21:51:55 +0330

    session 6

--- base/views.py
+++ base/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from .models import Product
 from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 def homePage(request):
-    products = Product.objects.all()
+    
 
     # filtered_products = Product.objects.filter(id=1)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    products = Product.objects.filter(Q(title__icontains = q) | Q(description__icontains = q))
+    
 
+    category = request.GET.getlist('category')
+    if len(category) != 0:
+        products = products.filter(size__in = category)
+    price = request.GET.get('price') if request.GET.get('price') != None else '1000'
+
+    products = products.filter(price__lte = price)
+
+
+
+    
     context = {
 
         'products':products,
@@ -22,6 +36,7 @@ def details(request, pk):
         product = Product.objects.get(id=pk)
     except Product.DoesNotExist:
         return HttpResponse("Product not found")
+
 
 
     context = {
